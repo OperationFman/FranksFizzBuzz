dce39c4f16dde14471886fd67470a322a5397c27
Franklin Moon <franklin.m.moon@gmail.com>
2020-12-29 14:17:38 +1100

    Converted functions to object and added Buzz

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,9 +1,16 @@
-function fizzBuzz(number) {
-    if (number % 15 == 0) {
-        return "FizzBuzz"
-    };
-    return number;
+const fizzBuzz= {
+    fizzAndBuzz: function(number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz"
+        };
+        return number;
+    },
+    buzz: function(number) {
+        if (number % 5 == 0) {
+            return "Buzz"
+        };
+        return number;
+    }
 }
-
 
 module.exports = fizzBuzz
--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -2,9 +2,17 @@ const { TestScheduler } = require('jest');
 const fizzBuzz = require('./../fizzBuzz/fizzBuzz.js');
 
 test('FizzBuzz triggered when number is a multiple of 15', () => {
-    expect(fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz.fizzAndBuzz(15)).toBe('FizzBuzz');
 });
 
-test('FizzBuzz triggered when number is a multiple of 15', () => {
-    expect(fizzBuzz(9)).toBe(9);
+test('FizzBuzz successfully not triggered', () => {
+    expect(fizzBuzz.fizzAndBuzz(9)).toBe(9);
+});
+
+test('Buzz triggered when number is a multiple of 5', () => {
+    expect(fizzBuzz.buzz(20)).toBe('Buzz');
+});
+
+test('Buzz successfully not triggered', () => {
+    expect(fizzBuzz.buzz(9)).toBe(9);
 });
